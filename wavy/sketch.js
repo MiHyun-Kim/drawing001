9c698ad4e364715a7c6ef884b3b56f6b05c354d8
MiHyun Kim <48187887+MiHyun-Kim@users.noreply.github.com>
2025-04-22 10:51:22 -0400

    Add files via upload

---
+++ wavy/sketch.js
@@ -0,0 +1,66 @@
+let font;
+let fontSize = 64;
+let message = "I told him that I'm struggling to understand. And he said, it's because you're a woman and women don't comprehend quite as fast...And I got really upset and I left.";
+let pointsArray = [];
+let slider;
+
+function preload() {
+  font = loadFont('https://cdnjs.cloudflare.com/ajax/libs/topcoat/0.8.0/font/SourceCodePro-Regular.otf');
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight - 50);
+  textFont(font);
+  textSize(fontSize);
+  noStroke();
+  fill(0);
+
+  slider = createSlider(0, 30, 5, 0.1);
+  slider.position(20, height + 10);
+  slider.style('width', '200px');
+
+  generatePointsAcrossLines();
+}
+
+function generatePointsAcrossLines() {
+  pointsArray = [];
+  let x = 40;
+  let y = 80;
+
+  for (let i = 0; i < 1000; i++) { // Large number to fill screen
+    let char = message[i % message.length];
+    let charWidth = textWidth(char);
+
+    if (x + charWidth > width - 40) {
+      x = 40;
+      y += fontSize * 1.2;
+      if (y > height - 40) break;
+    }
+
+    let pts = font.textToPoints(char, x, y, fontSize, {
+      sampleFactor: 0.25,
+      simplifyThreshold: 0
+    });
+
+    pointsArray.push(pts);
+    x += charWidth + 5;
+  }
+}
+
+function draw() {
+  background(255);
+  let t = millis() / 1000;
+  let waveAmount = slider.value();
+
+  for (let pts of pointsArray) {
+    for (let pt of pts) {
+      let waveX = pt.x + sin(t * 3 + pt.y * 0.05) * waveAmount;
+      let waveY = pt.y + cos(t * 3 + pt.x * 0.05) * waveAmount;
+      ellipse(waveX, waveY, 2);
+    }
+  }
+
+  fill(0);
+  textSize(16);
+  text("Wave Amount", slider.x * 1.2 + slider.width, slider.y + 10);
+}
