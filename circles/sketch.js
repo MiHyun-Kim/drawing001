bb143217c909a08231cd4a8e465db8f7ee5d0f3b
MiHyun Kim <48187887+MiHyun-Kim@users.noreply.github.com>
2025-05-09 14:45:20 -0400

    Add files via upload

---
+++ circles/sketch.js
@@ -0,0 +1,18 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);  
+  background(220, 200, 200, 0);
+
+}
+
+function draw() {
+  
+//  noStroke();
+  stroke(200, 20, 20);
+  fill(206,226,68,90);
+  ellipse(mouseX,mouseY,40,40);
+}
+
+/*
+function mousePressed(){
+    background(220, 200, 200,0);
+}*/
