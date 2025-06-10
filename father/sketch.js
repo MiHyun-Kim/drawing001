ff52be0773c810b945dd43ffaa5b199a7fa59da2
MiHyun Kim <48187887+MiHyun-Kim@users.noreply.github.com>
2025-06-10 13:39:01 -0400

    Update sketch.js

--- father/sketch.js
+++ father/sketch.js
@@ -1,22 +1,22 @@
 let bottomImg, topImg;
-let buffer; // offscreen canvas to hold the color image
+let buffer; 
 
 let w = window.innerWidth;
 let h = window.innerHeight;
 
 function preload() {
-  bottomImg = loadImage('alejandro-3-1.png');            // Full-color image
-  topImg = loadImage('alejandro_outline_1.png');         // Black-and-white image
+  bottomImg = loadImage('alejandro-3-1.png');           
+  topImg = loadImage('alejandro_outline_1.png');        
 }
 
 function setup() {
   createCanvas(w, h);
 
-  // Create offscreen buffer to hold bottom image
-  buffer = createGraphics(w, h);
-  buffer.image(bottomImg, 0, 0, w, h); // draw color image to buffer
 
-  // Draw the black-and-white image on top
+  buffer = createGraphics(w, h);
+  buffer.image(bottomImg, 0, 0, w, h);
+
+
   image(topImg, 0, 0, w, h);
 
   noCursor();
@@ -24,7 +24,7 @@ function setup() {
 }
 
 function mouseDragged() {
-  // Copy from buffer (color image) onto canvas where the mouse is
+
   copy(buffer, mouseX, mouseY, 40, 40, mouseX, mouseY, 40, 40);
 }
 
@@ -33,7 +33,7 @@ function windowResized() {
   h = windowHeight;
   resizeCanvas(w, h);
 
-  // Recreate buffer and redraw everything
+ 
   buffer = createGraphics(w, h);
   buffer.image(bottomImg, 0, 0, w, h);
 
