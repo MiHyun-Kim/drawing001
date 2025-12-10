21023876bd48e4e75ed456d435153176926cc309
MiHyun Kim <48187887+MiHyun-Kim@users.noreply.github.com>
2025-12-10 15:22:07 -0500

    Update sketch.js

--- wavy/sketch.js
+++ wavy/sketch.js
@@ -17,7 +17,7 @@ function setup() {
 
   slider = createSlider(0, 25, 5, 0.1);
   let sliderWidth = 200;
-  slider.position(170, height-30);
+  slider.position(width/2, height-30);
   slider.style('width', '200px');
   slider.style('background', '#e0e0e0');    
   slider.style('accent-color', '#FEACD8'); 
