2d1167091e49f2423c4036bc848293a7db60f89b
MiHyun Kim <48187887+MiHyun-Kim@users.noreply.github.com>
2025-12-15 11:21:33 -0500

    Add files via upload

---
+++ change_typesize/sketch.js
@@ -0,0 +1,44 @@
+let sampleSize = 4;
+let camWidth = 320;
+let camHeight = 240;
+let proportion = camWidth / camHeight;
+
+let sizeSlider;
+let message = "When I, in my senior year of high school in the cybersecurity class, there was a group project that we all did. The idea was we were supposed to reach out to a company or a group and say, we would like to test your system. We would like to see if it's secure, no charge or anything like that. We just wanna attest for that, for asking permission to attempt to break in these different ways. We'll give you the results and also recommend ways you can improve things to make it better. We ended up forming two groups. In my group, I was also the president of the school's gaming and eSports club.";
+let messageIndex = 0;
+
+let myFont;
+
+function preload() {
+  myFont = loadFont('Velvelyne-Regular.ttf');
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  noStroke();
+
+  // Slider now controls text size
+  sizeSlider = createSlider(5, 150, 20); 
+  sizeSlider.position((width / 2) - (sizeSlider.width / 2), height-80);
+
+  textFont(myFont);
+}
+
+function draw() {
+  background(206, 226, 68);
+
+  let textSizeValue = sizeSlider.value();
+  textSize(textSizeValue);
+
+  let gridStep = textSizeValue; // grid spacing adjusts with type size
+  messageIndex = 0;
+
+  for (let y = 0; y < height; y += gridStep) {
+    for (let x = 0; x < width; x += gridStep) {
+      fill('#ed1c24');
+      text(message[messageIndex], x, y);
+      messageIndex++;
+      messageIndex %= message.length;
+    }
+  }
+}
