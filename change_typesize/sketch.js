1c150f23f4fdf3b691cf8ba38e45cccf874be4b6
MiHyun Kim <48187887+MiHyun-Kim@users.noreply.github.com>
2025-12-15 11:39:12 -0500

    Update sketch.js

--- change_typesize/sketch.js
+++ change_typesize/sketch.js
@@ -7,6 +7,8 @@ let sizeSlider;
 let message = "When I, in my senior year of high school in the cybersecurity class, there was a group project that we all did. The idea was we were supposed to reach out to a company or a group and say, we would like to test your system. We would like to see if it's secure, no charge or anything like that. We just wanna attest for that, for asking permission to attempt to break in these different ways. We'll give you the results and also recommend ways you can improve things to make it better. We ended up forming two groups. In my group, I was also the president of the school's gaming and eSports club.";
 let messageIndex = 0;
 
+let sliderWidth = 400; 
+
 let myFont;
 
 function preload() {
@@ -17,8 +19,9 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   noStroke();
 
-  // Slider now controls text size
+
   sizeSlider = createSlider(5, 150, 20); 
+  sizeSlider.style('width', sliderWidth + 'px');
   sizeSlider.position((width / 2) - (sizeSlider.width / 2), height-80);
 
   textFont(myFont);
@@ -30,7 +33,7 @@ function draw() {
   let textSizeValue = sizeSlider.value();
   textSize(textSizeValue);
 
-  let gridStep = textSizeValue; // grid spacing adjusts with type size
+  let gridStep = textSizeValue; 
   messageIndex = 0;
 
   for (let y = 0; y < height; y += gridStep) {
